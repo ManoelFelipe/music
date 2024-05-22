6c36e31c500322872ce235e2ae3cd40285e808f4
ManoelFelipe <mfcf86@gmail.com>
2024-05-22 17:06:31 -0300

    commit

--- springbootapi/src/main/java/com/evertonogura/springbootapi/exception/ArtistaAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.evertonogura.springbootapi.exception;
-
-public class ArtistaAlreadyExistsException extends RuntimeException {
-	private static final long serialVersionUID = -1078732418380696153L;
-
-	public ArtistaAlreadyExistsException(String nome) {
-		super("Artista já existe. Nome: " + nome);
-	}
-}
